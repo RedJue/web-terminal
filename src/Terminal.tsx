6499ef54a0b2fb05bf7609cd6c03585df0460a60
redjue <pingfj77@gmail.com>
2020-12-24 14:51:09 +0800

    ✨feat(init): version 1.0.0

--- src/Terminal.tsx
+++ src/Terminal.tsx
@@ -1,13 +1,44 @@
 import { Terminal } from "xterm";
 import "xterm/css/xterm.css";
-import { useEffect } from "react";
-import React from "react";
+import { useCallback, useEffect, useRef } from "react";
+import { AttachAddon } from "xterm-addon-attach";
+import axios from "axios";
+
+const socketURL = "ws://127.0.0.1:4000/socket/";
 function VscodeTerminal() {
+  //初始化当前系统环境，返回终端的 pid，标识当前终端的唯一性
+  const initSysEnv = async (term: Terminal) =>
+    await axios
+      .post("http://127.0.0.1:4000/terminal", {
+        cols: term.cols,
+        rows: term.rows,
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw new Error(err);
+      });
+
   useEffect(() => {
-    var term = new Terminal();
+    var term = new Terminal({
+      fontFamily: 'Menlo, Monaco, "Courier New", monospace',
+      fontWeight: 400,
+      fontSize: 14,
+      rows: 200,
+    });
     //@ts-ignore
     term.open(document.getElementById("terminal"));
-    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+    term.focus();
+    async function asyncInitSysEnv() {
+      const pid = await initSysEnv(term),
+        ws = new WebSocket(socketURL + pid),
+        attachAddon = new AttachAddon(ws);
+      term.loadAddon(attachAddon);
+    }
+    asyncInitSysEnv();
+    return () => {
+      //组件卸载，清除 Terminal 实例
+      term.dispose();
+    };
   }, []);
   return <div id="terminal"></div>;
 }
---
+++ src/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const expressWs = require("express-ws");
+const pty = require("node-pty");
+const os = require("os");
+
+const shell = os.platform() === "win32" ? "powershell.exe" : "zsh";
+const app = express();
+expressWs(app);
+const termMap = new Map();
+function nodeEnvBind(req) {
+  const { cols, rows } = req.params;
+  //绑定当前系统 node 环境
+  const term = pty.spawn(shell, ["--login"], {
+    name: "xterm-color",
+    cols: cols || 80,
+    rows: rows || 24,
+    cwd: process.env.HOME,
+    env: process.env,
+  });
+  termMap.set(term.pid, term);
+  return term;
+}
+//解决跨域问题
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+//服务端初始化
+app.post("/terminal", (req, res) => {
+  const term = nodeEnvBind(req);
+  res.send(term.pid.toString());
+  res.end();
+});
+
+app.ws("/socket/:pid", (ws, req) => {
+  const pid = parseInt(req.params.pid);
+  const term = termMap.get(pid);
+  term.on("data", function (data) {
+    ws.send(data);
+  });
+
+  ws.on("message", (data) => {
+    console.log(typeof data === "string");
+    term.write(data);
+  });
+  ws.on("close", function () {
+    term.kill();
+    termMap.delete(pid);
+  });
+});
+app.listen(4000, "127.0.0.1");
